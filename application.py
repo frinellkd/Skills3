cce6fd68fc6efe5b3867c08632eb071d6721e1ca
U-KIMBERLYSLAPTOP\Kimberly <frinellkd@comcast.net>
2015-07-18 23:45:34 -0700

    Done with everything except advanced problems

--- application.py
+++ application.py
@@ -1,16 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index_page():
-    # Return this as a strange
-    return "<html><body>This is the homepage.</body></html>"
+@app.route("/application-form")
+def start():
+    
+    return render_template("application-form.html")
 
-    # Alternately, we could make this a Jinja template in `templates/`
-    # and return that result of rendering this, like:
-    #
-    # return render_template("index.html")
+@app.route("/application", methods=["POST"])
+def applicationForm():    
+    f_name = request.form.get("first_name")
+    l_name = request.form.get("last_name")
+    salary = request.form.get("salary_requirement")
+    job = request.form.get("job_type")
+
+    return render_template("application.html", first_name=f_name, last_name=l_name, salary=salary, job=job )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
